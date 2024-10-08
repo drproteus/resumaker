b85725263dd76dd5a20ff9d32e2042c23ee2c54b
Jake Goritski <jgoritski@headspin.io>
2024-10-08 11:43:48 -0700

    add pdf output

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import pdfgen
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
@@ -12,9 +13,29 @@ def render():
     with open("./data/resume.json", "r") as f:
         data = json.load(f)
     html = template.render(data)
+    return html
+
+
+def write(html):
     with open("./build/resume.html", "w") as f:
         f.write(html)
 
 
+def write_pdf(html):
+    options = {
+        "scale": 1.0,
+        "format": "Letter",
+        "margin": {
+            "top": "0.75in",
+            "right": "0.75in",
+            "bottom": "0.75in",
+            "left": "0.75in",
+        }
+    }
+    pdfgen.sync.from_string(html, "./build/resume.pdf", options=options)
+
+
 if __name__ == "__main__":
-    render()
+    html = render()
+    write(html)
+    write_pdf(html)
