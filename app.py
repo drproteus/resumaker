fd00808bd0ccbe23ccd5f7ecba42b7fe56c1ee52
Jake Goritski <jgoritski@headspin.io>
2024-10-08 11:58:16 -0700

    add constants

--- app.py
+++ app.py
@@ -4,36 +4,42 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
     loader=PackageLoader("app"),
-    autoescape=select_autoescape()
+    autoescape=select_autoescape(),
 )
+
+PDF_OPTIONS = {
+    "scale": 0.8,
+    "format": "Letter",
+    "margin": {
+        "top": "0.50in",
+        "right": "0.50in",
+        "bottom": "0.50in",
+        "left": "0.50in",
+    },
+    "pageRanges": "1",
+}
+
+RESUME_TEMPLATE = "resume.html"
+RESUME_JSON_PATH = "./data/resume.json"
+RESUME_HTML_PATH = "./build/resume.html"
+RESUME_PDF_PATH = "./build/resume.pdf"
 
 
 def render():
-    template = env.get_template("resume.html")
-    with open("./data/resume.json", "r") as f:
+    template = env.get_template(RESUME_TEMPLATE)
+    with open(RESUME_JSON_PATH, "r") as f:
         data = json.load(f)
     html = template.render(data)
     return html
 
 
 def write(html):
-    with open("./build/resume.html", "w") as f:
+    with open(RESUME_HTML_PATH, "w") as f:
         f.write(html)
 
 
 def write_pdf(html):
-    options = {
-        "scale": 0.8,
-        "format": "Letter",
-        "margin": {
-            "top": "0.50in",
-            "right": "0.50in",
-            "bottom": "0.50in",
-            "left": "0.50in",
-        },
-        "pageRanges": "1"
-    }
-    pdfgen.sync.from_string(html, "./build/resume.pdf", options=options)
+    pdfgen.sync.from_string(html, RESUME_PDF_PATH, options=PDF_OPTIONS)
 
 
 if __name__ == "__main__":
