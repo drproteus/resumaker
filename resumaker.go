282f2a456a78419a14898021cd27f9b8359d2ba8
Jake Goritski <jgoritski@gmail.com>
2026-01-26 18:14:03 -0800

    golang version

---
+++ resumaker.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"os"
+)
+
+type ResumeDataExp struct {
+	Title      string   `json:"title"`
+	Company    string   `json:"company"`
+	Location   string   `json:"location"`
+	StartDate  string   `json:"start_date"`
+	EndDate    string   `json:"end_date"`
+	Highlights []string `json:"highlights"`
+}
+
+type ResumeDataEdu struct {
+	School     string   `json:"school"`
+	Degree     string   `json:"degree"`
+	Highlights []string `json:"highlights"`
+}
+
+type ResumeData struct {
+	Fullname   string          `json:"fullname"`
+	Email      string          `json:"email"`
+	Website    string          `json:"website"`
+	Experience []ResumeDataExp `json:"experience"`
+	Education  []ResumeDataEdu `json:"education"`
+	Keywords   []string        `json:"keywords"`
+	Telephone  string          `json:"telephone"`
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Insufficient arguments!")
+		return
+	}
+	resumeDataPath := os.Args[1]
+	resumeTemplatePath := os.Args[2]
+
+	jsonData, _ := os.ReadFile(resumeDataPath)
+	var resumeData ResumeData
+	json.Unmarshal(jsonData, &resumeData)
+
+	var tpl *template.Template
+	tpl = template.Must(template.ParseFiles(resumeTemplatePath))
+	tpl.Execute(os.Stdout, resumeData)
+}
